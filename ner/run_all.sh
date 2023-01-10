227d4c4e0bea3284baf648708746dd82eb0c1e96
zaidalyafeai <alyafey22@gmail.com>
2023-01-10 09:58:10 +0300

    add  run all

---
+++ ner/run_all.sh
@@ -0,0 +1,32 @@
+train_path=$( echo $1 | tr '.' '_' )_train
+zero_path=$( echo $1 | tr '.' '_' )_zero_ar
+
+python run_ner.py \
+      --model_name_or_path bert-base-multilingual-cased \
+      --dataset_name xtreme \
+      --do_train \
+      --do_eval \
+      --per_device_train_batch_size 12 \
+      --learning_rate 3e-5 \
+      --num_train_epochs 2 \
+      --output_dir /tmp/$train_path \
+      --dataset_config_name $1
+
+python run_ner.py \
+      --model_name_or_path /tmp/$train_path \
+      --dataset_name xtreme \
+      --do_eval \
+      --per_device_train_batch_size 12 \
+      --learning_rate 3e-5 \
+      --num_train_epochs 2 \
+      --output_dir /tmp/$zero_path \
+      --dataset_config_name PAN-X.ar
+
+mkdir /content/$train_path
+mkdir /content/$zero_path
+
+cp /tmp/$train_path/all_results.json /content/$train_path/all_results.json
+cp /tmp/$zero_path/all_results.json /content/$zero_path/all_results.json
+
+rm -rf /tmp/$train_path
+rm -rf /tmp/$zero_path
